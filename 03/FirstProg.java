8348bf853b601a5f136c6fb0d36863dfbfc1e455
ZShammas-Toma <36395774+ZShammas-Toma@users.noreply.github.com>
2018-02-15 10:46:58 -0500

    Add files via upload

---
+++ 03/FirstProg.java
@@ -0,0 +1,7 @@
+public class FirstProg 
+{
+	public static void main(String[] args)
+{
+		System.out.println("You either die a hero, or you live long enough to see yourself become the villian");
+	}
+}
