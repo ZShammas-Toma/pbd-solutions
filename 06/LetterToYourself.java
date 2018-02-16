8e422596371a4ea0afa8839892cdaff2ff3896b3
ZShammas-Toma <36395774+ZShammas-Toma@users.noreply.github.com>
2018-02-16 10:10:40 -0500

    Update LetterToYourself.java

--- 06/LetterToYourself.java
+++ 06/LetterToYourself.java
@@ -1,7 +1,6 @@
 public class LetterToYourself
 {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 	System.out.println("+--------------------------------------------------------------------------+");	
 	System.out.println("|                                                                     #### |");
 	System.out.println("|                                                                     #### |");
